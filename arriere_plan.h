7c4da6ce670ccdee5bc0cc038849a3e775532a4e
habbak <taous.habbak@etu.u-paris.fr>
2023-12-20 01:23:55 +0100

    kill

--- arriere_plan.h
+++
@@ -1,9 +0,0 @@
-#ifndef ARRIERE_PLAN_H
-#define ARRIERE_PLAN_H
-
-
-int is_cmdArrierePlan(char **args,int nb_args) ;
-int modifie_args(char **args, int nb_args,char **chaine);
-int cmdArrierePlan (char **args,int nombre_jobs,struct Job  tab_jobs[],int nb_args, size_t len);
-
-#endif
