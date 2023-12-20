d1fd9af444c1c9eff8645bce25d71090967f89d6
Melissa <aklimeli24@gmail.com>
2023-12-20 14:37:52 +0100

    Adapter les redirections pour traiter les redirection multiples

--- redirections.h
+++ redirections.h
@@ -3,6 +3,12 @@
 
 extern int exit_code;
 
+typedef struct
+{
+    char *redirection;
+    char *redirectionFileName;
+} Redirection;
+
 int lecture(char *fic);
 int sans_ecrasement_stdout(char *fic);
 int avec_ecrasement_stdout(char *fic);
@@ -10,10 +16,14 @@ int concat_stdout(char *fic);
 int concat_stderr(char *fic);
 int sans_ecrasement_stdin(char *fic);
 int avec_ecrasement_stdin(char *fic);
-void is_redirection(char *commandline, int *index, char **redirection);
+int token_is_redirection(char *token);
+int commandline_is_redirection(char *commandline);
+void extract_redirections(char *commandline, Redirection **redirections, int *erreur, int *nb_redirections);
 char *extractCommandAndArgs(const char *commandLine, int index);
-char *extractRedirectionFileName(const char *commandLine, int index);
+// void is_redirection(char *commandline, int *index, char **redirection);
+// char *extractRedirectionFileName(const char *commandLine, int index);
 int execute_redirection(char *redirection, char *redirectionFileName);
 void reset_redirections(int stdin_copy, int stdout_copy, int stderr_copy);
+int execute_redirections(Redirection *redirections, int nb_redirections);
 
 #endif
